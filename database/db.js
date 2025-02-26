583502d61a787c280f96524b83bd0aeff47ab40e
Jasmin2208 <jasminkorat204@gmail.com>
2025-02-26 16:15:03 +0530

    Add database PORT on connection

--- database/db.js
+++ database/db.js
@@ -4,7 +4,8 @@ const knex = require('knex')({
         host: process.env.MYSQL_HOSTNAME,
         user: process.env.MYSQL_USER,
         password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE
+        database: process.env.MYSQL_DATABASE,
+        port: process.env.MYSQL_PORT,
     }
 });
 
